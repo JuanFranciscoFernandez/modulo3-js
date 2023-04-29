dda202dff85c67b6eac8145a60124895d5d3d3e8
JuanFranciscoFernandez <juanfranciscofernandezrrhh@gmail.com>
2023-04-28 22:57:35 -0300

    funcionesjss

---
+++ Funciones.js
@@ -0,0 +1,22 @@
+function sumaTres(x) {
+    console.log(x + 3);
+}
+sumaTres(5);
+
+//formas de escribir la misma funcion:
+
+//primera forma
+function sumaTres(x) {
+    return x + 3;
+}
+
+//segunda forma
+var sumaTres = function (x) {
+    return x + 3;
+}
+
+//Tercera Forma (funcion de flecha)
+var sumaTres = (X) => {
+    return X + 3;
+}
+
---
+++ Variables.js
@@ -0,0 +1,18 @@
+// string
+var bootcamp = "henry"
+
+//números
+var numeroentero = 17;
+var numeronegativo = -4;
+var numerodecimal = 11.75;
+
+//booleanos
+var estoyentendiento = true;
+var estoyaburrido = false;
+
+//undefined
+var cajon1;
+console.log(cajon1);
+
+//null
+var sinnada = null;
