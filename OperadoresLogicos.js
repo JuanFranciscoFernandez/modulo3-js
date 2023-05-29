f8c96b7769e31fc0e7f1e9c59e880036187abfb3
JuanFranciscoFernandez <juanfranciscofernandezrrhh@gmail.com>
2023-05-29 19:59:44 -0300

    costo pero puede aprender la precedencia en AND, OR, NOT

--- OperadoresLogicos.js
+++ OperadoresLogicos.js
@@ -30,3 +30,12 @@ condicionCompleja(10)
 condicionCompleja(6)
 condicionCompleja(3)
 condicionCompleja(5)
+
+/* Primero se resuelve la operacion del AND y despues la del OR
+
+num > 9 && num % 2 === 0 || num === 3 
+
+*/
+
+
+var = PutoElQueLoLee
