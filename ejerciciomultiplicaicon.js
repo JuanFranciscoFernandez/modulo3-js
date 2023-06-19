593d8f4effae297dc9f3e65464b33754a50ca7af
JuanFranciscoFernandez <juanfranciscofernandezrrhh@gmail.com>
2023-06-19 17:11:50 -0300

    estoy aprendiendo mucho

---
+++ ejerciciomultiplicaicon.js
@@ -0,0 +1,12 @@
+function multiplicarArgumentos() {
+    // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto.
+    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
+    // [PISTA]: "arguments" es un arreglo.
+    // Tu código:
+
+    var mayoresque18 = 0
+    for (var i = 0; i < array.length; i++) {
+       if (array[i] > 18) { mayoresque18=array[i] }
+    }
+    return mayoresque18
+}
