99f2310a285ad0314bd886f3ec64cced6cf6a43a
JuanFranciscoFernandez <juanfranciscofernandezrrhh@gmail.com>
2023-07-03 23:20:30 -0300

    prototipos

---
+++ Clases y prototipos.js
@@ -0,0 +1,43 @@
+//clases
+
+// ;;;;;Estilo function:
+
+let Animal = function (nombre, edad, color) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.color = color;
+    //;;;;;;;;metodo
+    this.duerme = function () {
+        console.log('zzzzzzzz')
+    };
+    this.caga = function () {
+        console.log(this.nombre + ' ' + "se esta echando terrible cago " + "y despues se va a dormir" + ' ' + this.duerme)
+    }
+}
+//;;;;;;;;;;;;;Nueva INSTANCIA
+let pato = new Animal('cachilo', 25, 'violeta')
+pato.caga()
+
+
+
+//estilo class:
+class Animal2 {
+    constructor(nombre, edad, color) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.color = color;
+    }
+    duerme = function () {
+        console.log("cagando")
+    }
+}
+
+var cucaracha = new Animal2("eureka", 10, "marron")
+
+
+//::::::P r o t o t i p o s ::::::::::::
+//se colocan por afuera, a lo ultimo de todo
+
+Animal2.prototype.caga = function(){
+    console.log('pufff cagando')
+}
---
+++ ejemplo.js
@@ -0,0 +1,19 @@
+class Animal2 {
+    constructor(nombre, edad, color) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.color = color;
+    }
+    // //duerme = function () {
+    //     console.log("Hola mi nombre es" + ' ' + this.nombre + " " + "y" + " " + "tengo" + ' ' + this.edad + " " + "anios")
+    // }
+}
+let cucaracha = new Animal2('eureka', 10, 'marron caca')
+
+
+//::::::P r o t o t i p o s ::::::::::::
+
+Animal2.prototype.caga = function () {
+    console.log(this.nombre + " " + 'caga y se tira muchos pedoss puuuuuhchh')
+}
+cucaracha.caga()
