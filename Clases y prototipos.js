a612a6239d1b6be236843b310b344741b928f981
nuevacompucommite <juanfranciscofernandezrrhh@gmail.com>
2023-10-24 23:13:05 -0300

    apusheo

--- Clases y prototipos.js
+++ Clases y prototipos.js
@@ -40,7 +40,7 @@ Animal2.prototype.caga = function () {
     console.log('pufff cagando')
 }
 
-//Extencioon de clases
+//Extencion de clases
 class Persona {
     constructor(nombre, edad) {
         this.nombre = nombre;
