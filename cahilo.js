07874cbdf415f3b804e1e719d01c1bbd54dbabde
nuevacompucommite <juanfranciscofernandezrrhh@gmail.com>
2023-12-13 23:03:42 -0300

    pre entrevisdta

--- cahilo.js
+++ cahilo.js
@@ -1,1 +1,16 @@
-ggfhgvdfdf
+function saludar (saludo){
+    return function (nombre){
+        console.log(saludo + " " + nombre)
+    }
+}
+
+//console.log(saludar('hola'))
+    
+var contenedor = saludar('hola');
+
+contenedor('juan');
+contenedor('cachilo');
+
+var contenedor2 = saludar('hello Sr ')
+
+contenedor2('raul')
